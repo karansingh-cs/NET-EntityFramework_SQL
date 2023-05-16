6236ab515630821f2bc886450947cecb6a7fb5dc
karansingh-cs <karanbir97@outlook.com>
2023-05-16 18:07:33 -0500

    Add project files.

---
+++ EFCoreRelationships/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreRelationships
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RpgClass { get; set; } = "knight";
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ EFCoreRelationships/Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreRelationships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult <List<Character>> > Get(int userId) 
+        {
+            var characters = await _context.Characters
+                .Where(c => c.UserId == userId)
+                .ToListAsync();
+
+            return characters;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> Create(Character character)
+        {
+           
+           var result =  _context.Characters.Add(character);
+           await  _context.SaveChangesAsync();
+            return await Get(character.UserId);
+        }
+    }
+}
